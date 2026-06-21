41f5ba105e2c4302e8d67c08b58ded1a49c4943b
阿柴 <113411832+a-chaii@users.noreply.github.com>
2026-06-21 23:05:19 +0800

    Fix intensityMultiplier type conversion

--- app/src/main/java/com/hapticaudio/XposedEntry.kt
+++ app/src/main/java/com/hapticaudio/XposedEntry.kt
@@ -35,7 +35,7 @@ class XposedEntry : IXposedHookLoadPackage {
                 if (now - lastPrefReloadTime > 1000) {
                     pref.reload()
                     isMuted = pref.getBoolean("mute_speaker", true)
-                    AudioProcessor.intensityMultiplier = pref.getInt("intensity", 100f) / 100f
+                    AudioProcessor.intensityMultiplier = pref.getInt("intensity", 100) / 100f
                     lastPrefReloadTime = now
                 }
 
