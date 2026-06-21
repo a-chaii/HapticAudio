755722d6d8f1172b311f24c76043cb4b5075cef6
阿柴 <113411832+a-chaii@users.noreply.github.com>
2026-06-21 21:11:51 +0800

    Add global build script for Android project

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+// 项目全局构建脚本
+plugins {
+    // 定义 Android 编译插件版本
+    id("com.android.application") version "8.2.2" apply false
+    // 定义 Kotlin 插件版本
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
