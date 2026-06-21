15301b5f0da1056a7564ec5f8696bd2aab2c36d3
阿柴 <113411832+a-chaii@users.noreply.github.com>
2026-06-21 20:49:00 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        // 【必须添加】Xposed 官方 Maven 仓库
+        maven { url = uri("https://api.xposed.info/") }
+    }
+}
+rootProject.name = "HapticAudio"
+include(":app")
 
